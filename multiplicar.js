bab806e99f54ecaffff7e4d97355385730c2130b
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-23 17:14:00 -0500

    feat: tabla funcional usando js

---
+++ multiplicar.js
@@ -0,0 +1,23 @@
+let titulo = document.getElementById("titulo");
+let contenedor = document.getElementById("cuerpo-tabla");
+
+function tablaMultiplicadora(valor) {
+  let contenido = "";
+
+  for (let i = 1; i <= 10; i++) {
+    let resultado = i * valor;
+
+    contenido += `
+      <tr>
+        <td>${valor} × ${i}</td>
+        <td>${resultado}</td>
+      </tr>
+    `;
+  }
+  titulo.innerText = `Tabla del ${valor}`;
+  contenedor.innerHTML = contenido;
+}
+
+document.getElementById('btnProbar').addEventListener('click', () => {
+  tablaMultiplicadora(5);
+});
